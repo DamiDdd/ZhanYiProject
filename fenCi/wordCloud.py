a22d650895c2f8857196bb634275197296910a6f
e0392448 <834767813@qq.com>
2020-05-17 11:23:26 +0800

    Wordcloud

--- fenCi/wordCloud.py
+++ fenCi/wordCloud.py
@@ -1,16 +1,20 @@
-from wordcloud import WordCloud
+from wordcloud import WordCloud, ImageColorGenerator
 import matplotlib.pyplot as plt
 import  pkuFenci
 
 # 生成词云
-def create_word_cloud(frequencies, filename, encoding, font_path):
+def create_word_cloud(frequencies, font_path, mask_image):
+    mask = plt.imread(mask_image)
     wc = WordCloud(
         font_path=font_path,
         max_words=100,
         width=2000,
         height=1200,
+        background_color="white",
+        mask=mask,
     )
     word_cloud = wc.generate_from_frequencies(frequencies)
+    word_cloud.recolor(color_func=ImageColorGenerator(mask))
     # 写词云图片
     word_cloud.to_file("wordcloud.jpg")
     # 显示词云文件
@@ -20,4 +24,4 @@ def create_word_cloud(frequencies, filename, encoding, font_path):
 
 if __name__ == '__main__':
     dict = pkuFenci.get_dict()
-    create_word_cloud(dict,"./record.txt","utf-8","C:\WINDOWS\FONTS\STKAITI.TTF")
+    create_word_cloud(dict,"C:\WINDOWS\FONTS\STKAITI.TTF", "./mask.jpg")
